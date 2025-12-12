950824dc8fe75defab86e74def6a6c8a59c5d868
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-12-12 22:12:49 +0530

    Prototype scan and linking done for UPI

--- App.tsx
+++ App.tsx
@@ -1,45 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, Linking } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+  useCodeScanner,
+} from 'react-native-vision-camera';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const device = useCameraDevice('back');
+  const { hasPermission, requestPermission } = useCameraPermission();
+  const [scanned, setScanned] = useState(false);
+
+  const upiDeepLink = async (upi_url: string) => {
+    if (!upi_url.startsWith('upi://')) return;
+    const supported = await Linking.canOpenURL(upi_url);
+    if (supported) {
+      Linking.openURL(upi_url);
+    }
+  };
+
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission]);
+
+  const upiScanner = useCodeScanner({
+    codeTypes: ['qr'],
+    onCodeScanned: async codes => {
+      if (!scanned && codes.length > 0) {
+        setScanned(true);
+
+        const value = codes[0]?.value ?? 'Unknown';
+
+        upiDeepLink(value);
+        setTimeout(() => setScanned(false), 2000);
+      }
+    },
+  });
+
+  if (device == null) return <Text>No device</Text>;
+  if (!hasPermission) return <Text>Waiting for camera permission</Text>;
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive
+      codeScanner={upiScanner}
+    />
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
